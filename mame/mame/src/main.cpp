c6727c4a50fec0d4e160545abadf9cc041cf7db9
simon.huber17 <simon.huber17@gmail.com>
2018-04-29 12:12:16 +0200

    created project

---
+++ mame/mame/src/main.cpp
@@ -0,0 +1,33 @@
+
+class ime
+{
+private:
+
+
+
+
+
+public:
+
+
+
+
+};
+
+
+
+
+
+int main()
+{
+	/*
+	this is how the code should be useable:
+
+	ime big_number({2,4,56,7,43,8}); //2^4^56^7^43^8
+	int small_number = 756;
+	int smaller_number = big_number % small_number;
+	
+	*/
+
+
+}
